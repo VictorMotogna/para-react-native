50bb09ffad50eaa72f7c66b2916542281eae4a7d
victormotogna <vmotogna@gmail.com>
2017-11-17 18:28:35 +0200

    added mail function, needs ui & list update fix

--- src/components/expense/Expense.js
+++ src/components/expense/Expense.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TextInput, TouchableOpacity, Linking} from 'react-native';
 
 const util = require('util');
 
@@ -8,10 +8,46 @@ export default class Expense extends Component {
         title: 'Expense',
     };
 
+    sendEmail() {
+        let url = "mailto:";
+        url += "?subject=";
+        url += "&body=";
+        Linking.openURL(url);
+    }
+
+    editExpense() {
+        var { params: { expenses, id, onEditExpense} } = this.props.navigation.state;
+        if (onEditExpense && typeof onEditExpense === 'function') {
+            onEditExpense(id , `${expenses}edited`);
+        }
+
+        this.props.navigation.navigate("ExpenseList", { expenses, id, onEditExpense: this.onEditExpense } );
+    }
+
     render() {
-        var {params} = this.props.navigation.state;
+        var { params: { expenses, id, onEditExpense} } = this.props.navigation.state;
+
         return(
-            <Text>{params.expense}</Text>
+            <View>
+                <TextInput placeholder={expenses}/>
+
+                <TouchableOpacity
+                    onPress={
+                        // () => this.props.navigation.navigate("ExpenseList")
+                        this.props.expenses =
+                        () => this.editExpense()
+                    }>
+                    <Text>Save</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={
+                        // () => this.props.navigation.navigate("ExpenseList")
+
+                        () => this.sendEmail()
+                    }>
+                    <Text>mail</Text>
+                </TouchableOpacity>
+            </View>
         );
     }
 }
--- src/components/expense/ExpenseList.js
+++ src/components/expense/ExpenseList.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, ListView, Text, TouchableHighlight} from 'react-native';
+import {View, ListView, Text, TouchableHighlight, ScrollView} from 'react-native';
 
 const util = require('util');
 
@@ -7,39 +7,55 @@ var expenseArray = ["Food", "Drinks", "Gift", "Jacket"];
 
 export default class ExpenseList extends Component {
     static navigationOptions = {
-        title: 'Add expense screen',
+        title: 'Expenses screen',
     };
 
     constructor(props) {
         super(props);
+        expenseArray = ["Food", "Drinks", "Gift", "Jacket"];
         var dataSource = new ListView.DataSource({rowHasChanged:(r1,r2) => r1.guid != r2.guid});
-        this.state = {dataSource: dataSource.cloneWithRows(expenseArray)}
+        this.state = {dataSource: dataSource.cloneWithRows(expenseArray)};
     }
 
-    renderRow(rowData, sectionID, rowID) {
-        var {navigate} = this.props.navigation;
-        return(
-            <TouchableHighlight
-                underlayColor='#ddd'
-                style={{height:44}}
-                onPress={
-                    () => navigate("Expense", {expense: rowData})
-                }>
-                <View>
-                    <Text style={{fontSize:20, color:'#000'}} numberOfLines={1}>{rowData}</Text>
-                    <View style={{height:1, backgroundColor:'#000', opacity: 0.1}}/>
-                </View>
-            </TouchableHighlight>
-        );
+    openPage(expenses, id) {
+        this.props.navigation.navigate("Expense", { expenses, id, onEditExpense: this.onEditExpense } );
     }
+
+    onEditExpense = (id, newExpense) => {
+        console.log(' onEdit', id, newExpense, this.state.dataSource);
+        let newDs = [];
+        newDs = this.state.dataSource._dataBlob.s1.slice();
+        newDs[id].Selection = newExpense;
+        this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(newDs)
+        })
+    };
 
     render() {
         var {navigate} = this.props.navigation;
-
+        const array = expenseArray.map( (item, index) => {
+            const desc = expenseArray[index] ? <Text>{expenseArray[index]}</Text>:<View/>;
+            return(
+                <View key={index}>
+                    <View>
+                        <TouchableHighlight
+                            underlayColor='#ddd'
+                            style={{height:44}}
+                            onPress={this.openPage.bind(this, expenseArray[index], index)}>
+                            <View>
+                                <Text style={{fontSize:20, color:'#000'}} numberOfLines={1}>{expenseArray[index]}</Text>
+                                <View style={{height:1, backgroundColor:'#000', opacity: 0.1}}/>
+                            </View>
+                        </TouchableHighlight>
+                    </View>
+                </View>
+            )
+        });
         return(
-            <ListView dataSource={this.state.dataSource} renderRow={this.renderRow.bind(this)} style={{marginTop: 16}}>
-            </ListView>
-        );
+            <ScrollView>
+                {array}
+            </ScrollView>
+        )
     }
 }
 
